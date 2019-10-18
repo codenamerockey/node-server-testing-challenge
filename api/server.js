5dc41f90c4e333ddef3a694b75c3105cfc374d01
Rockey Simmons <codenamerockey@gmail.com>
2019-10-17 18:33:03 -0700

    created server to test endpoints and necessary dependencies

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hobbits = require('../database/hobs_model');
+const Hobbits = require('../database/hobs_model');
 
 const server = express();
 
@@ -10,9 +10,41 @@ server.get('/', async (req, res) => {
 });
 
 server.get('/api/hobbits', async (req, res) => {
-  const rows = await hobbits.getAll();
+  const rows = await Hobbits.getAll();
 
   res.status(200).json(rows);
+});
+
+server.post('/', (req, res) => {
+  const hobbits = req.body;
+
+  Hobbits.add(hobbits)
+    .then(hobbit => {
+      res.status(201).json(hobbit);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to add new hobbit to database' });
+    });
+});
+
+server.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Hobbits.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'Could not find hobbit with given id' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'Failed to delete hobbit from the database' });
+    });
 });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// const server = require('./api/server.js')
+require('dotenv').config();
 
-// const port = process.env.PORT || 8000;
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
